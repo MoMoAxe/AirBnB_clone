3aec42df16cf8b80d621d41fe7777bf1f612de19
MoMoAxe <tobioluwa348@gmail.com>
2023-07-15 14:47:56 -0700

    Fix

---
+++ sum.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """Function to add two numbers."""
+    return a + b
+
+
+def main():
+    """Main function that demonstrates the usage of the add function."""
+    sum = add(5, 7)
+    print(sum)
+
+
+if __name__ == '__main__':
+    main()
